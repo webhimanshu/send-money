4341a64ed860902cde1af8808a8efea8359dbec5
himanshu <himanshusharma2709@gmail.com>
2025-11-23 16:29:45 +0530

    login and signup api integreted

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,9 +2,13 @@ import { AppDataSource } from "./data-source";
 import express from "express";
 import authRoutes from "./routes/auth.routes";
 import transferRoutes from "./routes/transfer.routes";
+import cors from "cors";
 const app = express();
 app.use(express.json());
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 AppDataSource.initialize()
   .then(() => {
     console.log("DataSource initialized");
